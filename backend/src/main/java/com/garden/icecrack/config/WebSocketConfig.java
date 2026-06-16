1b15782cce18b014df65ce5d4499333b6a90deb0
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-17 07:29:52 +0800

    Update project

---
+++ backend/src/main/java/com/garden/icecrack/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.garden.icecrack.config;
+
+import com.garden.icecrack.alarm_ws.websocket.AlertWebSocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private AlertWebSocketHandler alertWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(alertWebSocketHandler, "/ws/alerts")
+                .setAllowedOrigins("*");
+    }
+}
